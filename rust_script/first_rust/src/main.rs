32eaf696cd66432bfac62a6d0f3392304dbf1cbb
Ethan <ethanhuang03@gmail.com>
2023-06-22 08:08:49 -0700

    scrape code4rena for github links and stores them in a vector

--- rust_script/first_rust/src/main.rs
+++ rust_script/first_rust/src/main.rs
@@ -1,10 +1,32 @@
-use scraper::Selector;
-use scraper::Html;
+use scraper::{ Html, Selector };
+use headless_chrome::Browser;
+use std::error::Error;
+use std::time::Duration;
+use std::thread;
 
 fn main() {
-    println!("cargo:rerun-if-changed=./.lalrpop");
     let url = "https://code4rena.com/contests";
-    let response = reqwest::blocking::get(url).expect("Could not load url");
-    let raw_html = response.text().unwrap();
-    println!("{}", raw_html);
+    let mut audits = Vec::new();
+    get_audit_links(url, &mut audits);
+    for element in audits {
+        println!("{:?}", element);
+    }
+}
+
+fn get_audit_links(url: &str, mut audits: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
+    let browser = Browser::default()?;
+    let tab = browser.new_tab()?;
+    
+    tab.navigate_to(&url)?;
+    thread::sleep(Duration::from_millis(1000));
+    
+    let raw_html = tab.get_content().unwrap();
+    let document = Html::parse_document(&raw_html);
+    let div_selector = Selector::parse(r#"a[aria-label="Go to audit competition repo (Opens in a new window)"]"#).unwrap();
+    
+    for element in document.select(&div_selector) {
+        audits.push(element.value().attr("href").unwrap().to_string());
+    }
+
+    Ok(())
 }
