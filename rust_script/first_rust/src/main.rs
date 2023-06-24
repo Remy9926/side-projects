0af47e94c996de47fb0fade84ffc1b7e9304a8f4
Ethan <ethanhuang03@gmail.com>
2023-06-24 16:47:10 -0400

    pull repo information from github api

--- rust_script/first_rust/src/main.rs
+++ rust_script/first_rust/src/main.rs
@@ -3,17 +3,23 @@ use headless_chrome::Browser;
 use std::error::Error;
 use std::time::Duration;
 use std::thread;
+use std::collections::HashMap;
+
+const GITHUB_API_URL: &str = "https://api.github.com/repos/code-423n4/";
 
 fn main() {
     let url = "https://code4rena.com/contests";
     let mut audits = Vec::new();
-    get_audit_links(url, &mut audits);
+    let _ = get_audit_links(url, &mut audits);
     for element in audits {
-        println!("{:?}", element);
+        let v: Vec<&str> = element.split("/").collect(); 
+        let github_url = GITHUB_API_URL.to_owned() + v[v.len() - 1] + "/contents";
+        println!("{}", github_url);
+        get_repo_info(&github_url)
     }
 }
 
-fn get_audit_links(url: &str, mut audits: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
+fn get_audit_links(url: &str, audits: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
     let browser = Browser::default()?;
     let tab = browser.new_tab()?;
     
@@ -29,4 +35,11 @@ fn get_audit_links(url: &str, mut audits: &mut Vec<String>) -> Result<(), Box<dy
     }
 
     Ok(())
+}
+
+fn get_repo_info(url: &str) {
+    let client = reqwest::blocking::Client::new();
+    let request_builder = client.request(reqwest::Method::GET, url).header("User-Agent", "remy9926");
+    let response = request_builder.send().unwrap().text().unwrap();
+    println!("{}", response);
 }
