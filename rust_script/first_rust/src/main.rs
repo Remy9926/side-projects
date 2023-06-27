e8789136fc4054a9ae548111263380e4c77cbace
Ethan <ethanhuang03@gmail.com>
2023-06-26 23:42:15 -0400

    search the entire repo for .sol files

--- rust_script/first_rust/src/main.rs
+++ rust_script/first_rust/src/main.rs
@@ -4,11 +4,13 @@ use std::error::Error;
 use std::time::Duration;
 use std::thread;
 use serde_json::{ Value };
+use dotenv::dotenv;
 
 const GITHUB_API_URL: &str = "https://api.github.com/repos/code-423n4/";
 static mut SOLIDITY_FILES: Vec<String> = Vec::new();
 
 fn main() {
+    dotenv().ok();
     let url = "https://code4rena.com/contests";
     let mut audits = Vec::new();
     let _ = get_audit_links(url, &mut audits);
@@ -16,11 +18,12 @@ fn main() {
         let v: Vec<&str> = element.split("/").collect(); 
         let github_url = GITHUB_API_URL.to_owned() + v[v.len() - 1] + "/contents";
         println!("{}", github_url);
-        get_repo_info(&github_url);
+        get_content_info(&github_url);
     }
     unsafe {
-        println!("{:?}", SOLIDITY_FILES.len());
+        println!("{:?}", SOLIDITY_FILES);
     }
+    //println!("{}", dotenv::var("GITHUB_API_KEY").unwrap());
 }
 
 fn get_audit_links(url: &str, audits: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
@@ -41,9 +44,13 @@ fn get_audit_links(url: &str, audits: &mut Vec<String>) -> Result<(), Box<dyn Er
     Ok(())
 }
 
-fn get_repo_info(url: &str) {
+fn get_content_info(url: &str) {
     let client = reqwest::blocking::Client::new();
-    let request_builder = client.request(reqwest::Method::GET, url).header("User-Agent", "remy9926");
+    let mut header_map = reqwest::header::HeaderMap::new();
+    header_map.insert("User-Agent", reqwest::header::HeaderValue::from_str("remy9926").unwrap());
+    header_map.insert("Authorization", reqwest::header::HeaderValue::from_str(&dotenv::var("GITHUB_API_KEY").unwrap()).unwrap());
+    
+    let request_builder = client.request(reqwest::Method::GET, url).headers(header_map);
     let response = request_builder.send().unwrap().text().unwrap();
     //response = response[1..response.len() - 1].to_string();
     let _ = parse_json(&response);
@@ -57,21 +64,15 @@ fn parse_json(response: &str) -> Result<(), Box<dyn Error>> {
         let file_type = file_json.unwrap()["type"].as_str().unwrap();
         if file_type == "file" {
             if file_json.unwrap()["name"].as_str().unwrap().ends_with(".sol") {
-                println!("Is a solidity file!");
+                println!("{}", file_json.unwrap()["url"].as_str().unwrap().to_string() + "is a solidity file!");
                 unsafe {
                     SOLIDITY_FILES.push(file_json.unwrap()["name"].as_str().unwrap().to_string() + " is a solidity file");
-                }
-            } else {
-                println!("Not a solidity file!");
-                unsafe {
-                    SOLIDITY_FILES.push(file_json.unwrap()["name"].as_str().unwrap().to_string() + " is not a solidity file!");
                 }
             }
         } else {
             let dir_url = file_json.unwrap()["url"].as_str().unwrap();
-            println!("{}", dir_url);
+            let _ = get_content_info(dir_url);
         }
-        println!("");
     }
     
     Ok(())
