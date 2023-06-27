eccabbe69cf6b9d3a828cc365f65c9e1b47a8f67
Ethan <ethanhuang03@gmail.com>
2023-06-27 19:50:13 -0400

    fn to get contract bytecode

--- rust_script/first_rust/src/main.rs
+++ rust_script/first_rust/src/main.rs
@@ -1,29 +1,43 @@
-use scraper::{ Html, Selector };
+use scraper::{Html, Selector};
 use headless_chrome::Browser;
 use std::error::Error;
 use std::time::Duration;
 use std::thread;
-use serde_json::{ Value };
+use serde_json::{Value};
 use dotenv::dotenv;
+use ethers_solc::{Project, ProjectPathsConfig};
+use std::fs;
+use std::io::prelude::*;
 
 const GITHUB_API_URL: &str = "https://api.github.com/repos/code-423n4/";
 static mut SOLIDITY_FILES: Vec<String> = Vec::new();
 
 fn main() {
-    dotenv().ok();
-    let url = "https://code4rena.com/contests";
-    let mut audits = Vec::new();
-    let _ = get_audit_links(url, &mut audits);
-    for element in audits {
-        let v: Vec<&str> = element.split("/").collect(); 
-        let github_url = GITHUB_API_URL.to_owned() + v[v.len() - 1] + "/contents";
-        println!("{}", github_url);
-        get_content_info(&github_url);
-    }
-    unsafe {
-        println!("{:?}", SOLIDITY_FILES);
-    }
-    //println!("{}", dotenv::var("GITHUB_API_KEY").unwrap());
+    let _ = get_contract_bytecode();
+    //let output: Vec<_> = project.compile().unwrap().into_contract_bytecodes().collect();
+    //println!("{:?}", output);
+    //let url = "https://raw.githubusercontent.com/code-423n4/2023-06-lybra/main/contracts/mocks/chainLinkMock.sol";
+    //let mut file = fs::File::create("./contracts/chainLinkMock.sol").unwrap();
+    //file.write_all(b"Henlo bera");
+    //let mut opened_file = fs::File::open("./contracts/chainLinkMock.sol").unwrap();
+    //let mut contents = String::new();
+    //opened_file.read_to_string(&mut contents).unwrap();
+    //println!("{}", contents);
+    //
+    //dotenv().ok();
+    //let url = "https://code4rena.com/contests";
+    //let mut audits = Vec::new();
+    //let _ = get_audit_links(url, &mut audits);
+    //for element in audits {
+    //    let v: Vec<&str> = element.split("/").collect(); 
+    //    let github_url = GITHUB_API_URL.to_owned() + v[v.len() - 1] + "/contents";
+    //    println!("{}", github_url);
+    //    get_content_info(&github_url);
+    //}
+    //unsafe {
+    //    println!("{:?}", SOLIDITY_FILES);
+    //}
+    //println!("{}", dotenv::var("GITHUB_API_KEY").unwrap()); 
 }
 
 fn get_audit_links(url: &str, audits: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
@@ -38,7 +52,7 @@ fn get_audit_links(url: &str, audits: &mut Vec<String>) -> Result<(), Box<dyn Er
     let aria_label_selector = Selector::parse(r#"a[aria-label="Go to audit competition repo (Opens in a new window)"]"#).unwrap();
     
     for element in document.select(&aria_label_selector) {
-        audits.push(element.value().attr("href").unwrap().to_string());
+        audits.push(element.value().attr("href").unwrap().to_owned());
     }
 
     Ok(())
@@ -60,20 +74,36 @@ fn parse_json(response: &str) -> Result<(), Box<dyn Error>> {
     let json: Value = serde_json::from_str(response).unwrap();
     for i in 0..json.as_array().unwrap().len() {
         let file_json = json.as_array().unwrap().get(i);
-        println!("{}", file_json.unwrap()["name"].as_str().unwrap());
         let file_type = file_json.unwrap()["type"].as_str().unwrap();
         if file_type == "file" {
             if file_json.unwrap()["name"].as_str().unwrap().ends_with(".sol") {
-                println!("{}", file_json.unwrap()["url"].as_str().unwrap().to_string() + "is a solidity file!");
+                println!("{}", file_json.unwrap()["name"].as_str().unwrap().to_owned() + " is a solidity file!");
                 unsafe {
-                    SOLIDITY_FILES.push(file_json.unwrap()["name"].as_str().unwrap().to_string() + " is a solidity file");
+                    SOLIDITY_FILES.push(file_json.unwrap()["name"].as_str().unwrap().to_owned());
                 }
             }
         } else {
+            println!("{}", file_json.unwrap()["name"].as_str().unwrap().to_owned() + " is not a solidity file");
             let dir_url = file_json.unwrap()["url"].as_str().unwrap();
             let _ = get_content_info(dir_url);
         }
     }
     
     Ok(())
+}
+
+fn get_contract_bytecode() { //push all solidity files into vector and see if they compile to some bytecode then figure out how to import
+    unsafe {
+        let project = Project::builder()
+        .paths(ProjectPathsConfig::hardhat("./contracts").unwrap())
+        .set_auto_detect(true)
+        .build()
+        .unwrap();
+        for file in &SOLIDITY_FILES {
+            let output = project.compile_file("./contracts/".to_owned() + file).unwrap(); //can replace the file with any file that you want
+            let get_bytecode: Vec<_> = output.into_contract_bytecodes().collect(); //since it compiles file by file, the size of this vector will be 1
+            let (_, compact_contract_bytecode) = &get_bytecode[0]; //extract the CompactContractBytecode object
+            println!("{:?}", compact_contract_bytecode.bytecode.clone().unwrap().object.into_bytes().unwrap().to_string()); //finally get the bytecode and print it out
+        }
+    }
 }
